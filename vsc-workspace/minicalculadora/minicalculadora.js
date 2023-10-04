4841ea92b20f81198644f8668424d10614b878c1
Ariel <arielaam77reducastur.es>
2023-10-04 14:20:12 +0200

    04/10/23

--- vsc-workspace/minicalculadora/minicalculadora.js
+++ vsc-workspace/minicalculadora/minicalculadora.js
@@ -12,7 +12,8 @@ window.onload = function(){
     let igual = document.querySelector(".igual");
     let borrar = document.querySelector("#borrar");
     let deci = document.querySelector("#decimal");
-
+    let unitario = document.querySelectorAll(".unitario");
+    let pi = document.querySelector("#pi");
     for (let iterator of digito) {
         iterator.addEventListener("click",numero);
         
@@ -22,18 +23,29 @@ window.onload = function(){
         iterator.addEventListener("click",operacion);
     
     }
+
+    for (let iterator of unitario){
+        iterator.addEventListener("click",resolverUnitario);
+    }
     deci.addEventListener("click",decimal);
     igual.addEventListener("click",resolver);
 
     borrar.addEventListener("click",borrado);
-    
+
+    pi.addEventListener("click",Pi);
+    function Pi(event){
+        sustituto = Math.PI.toString();
+        sustituto = sustituto.replace(".",",");
+        input.value = sustituto;
+        sustituto=0;
+    }
     function decimal(event){
         if (input.value.includes(",")){
             deci.removeEventListener;
         }
         else{
             if (input.value!=0){
-                input.value+=event.target.textContent;
+                actualizarDisplay(input.value+=event.target.textContent);
             }
         }
         
@@ -43,13 +55,38 @@ window.onload = function(){
         op1 = 0;
         operando = "";
     }
+
+    function resolverUnitario(event){
+        op1 = Number(input.value);
+
+        switch(event.target.textContent){
+            case "√":
+                actualizarDisplay(Math.sqrt(op1));
+                break;
+            case "x2":
+                actualizarDisplay(Math.pow(op1,2));
+                break;
+            case "sen":
+                actualizarDisplay(Math.sin(op1));
+                break;
+            case "cos":
+                actualizarDisplay(Math.cos(op1));
+                break;
+            case "tang":
+                actualizarDisplay(Math.tan(op1));
+        }
+    }
+
     function resolver(event){
         if (op1!=0){
             sustituto = input.value;
+            sustituto = sustituto.replaceAll(".","");
             op2 = parseFloat(sustituto.replace(",","."));
             sustituto = 0;
             resultado = `${op1} ${operando} ${op2}`;
-            input.value = String(eval(resultado));
+
+            actualizarDisplay(eval(resultado));
+            //input.value = String(eval(resultado));
         }
     }
 
@@ -61,6 +98,7 @@ window.onload = function(){
             operando = event.target.textContent;
         }
         sustituto = input.value;
+        sustituto = sustituto.replaceAll(".","");
         op1 = parseFloat(sustituto.replace(",","."));
         sustituto = 0;
         input.value="";
@@ -68,12 +106,25 @@ window.onload = function(){
 
     function numero(event){
         if (input.value == 0){
-            input.value = event.target.textContent;
+            actualizarDisplay(input.value = event.target.textContent);
         }
         else{
-            input.value = input.value + event.target.textContent;
+            actualizarDisplay(input.value += event.target.textContent);
         }
     }
 
+    function actualizarDisplay(cadena){
+        
+        if (typeof (cadena) === "string"){
+            if (!cadena.includes(",")){
+                cadena = cadena.replaceAll(".","");
+                cadena = Number(cadena).toLocaleString("de-DE");
+            }
+            else{
+                cadena = cadena.toLocaleString("de-DE");
+            }
+        }
+        input.value = cadena;
+    }
     
 }
