17bcb5f660b6008c7f398d7979a9f76e5693483c
Ariel <arielaam77@educastur.es>
2023-10-18 14:21:04 +0200

    18/10/23

--- pequeñoCustionario/procesadorespuestas.js
+++ pequeñoCustionario/procesadorespuestas.js
@@ -1,12 +1,17 @@
 window.onload = function() {
     let boton = document.querySelector("button");
-    
-
-    boton.addEventListener("click",procesadoRespuesta);
-
-    function procesadoRespuesta() {
-        let respuesta = document.querySelectorAll(".respuesta input[type=radio]").value==='true';
-        let color = (respuesta) ? "green" : "red";
-        document.querySelectorAll(".respuesta input[type=radio]:checkd+label").value.backgroundcolor=color;
-    }
+    let respuestas = document.querySelectorAll(".respuesta input[type=radio]");
+    boton.addEventListener("click",procesaRespuesta);
+            /*for (let respuesta of respuestas) {
+                respuesta.addEventListener("change", procesaRespuesta);
+            }*/
+            function procesaRespuesta1() {
+                console.log(document.querySelector(".respuesta input[type=radio]:checked").value) 
+            }
+            function procesaRespuesta() {
+                let respuesta = document.querySelector(".respuesta input[type=radio]:checked").value === "true";
+                let color = (respuesta) ? "green" : "red";
+                console.log(document.querySelector(".respuesta input[type=radio]:checked+label")) 
+                document.querySelector(".respuesta input[type=radio]:checked+label").style.backgroundColor=color
+            }
 }
