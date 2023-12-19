41435f0e5330c0b6fdfb0e431cf47421ffbec479
Ariel <arielaam77@educastur.es>
2023-12-19 11:11:59 +0100

    19/12/23

--- practicaJuego/codigo/js/ajedrez.js
+++ practicaJuego/codigo/js/ajedrez.js
@@ -1,6 +1,6 @@
 window.onload=function() {
 
-    
+    const letras = ["a","b","c","d","e","f","g","h"];
     let i = 0;
     let tablero = document.createElement("section");
     tablero.classList.add("principal");
@@ -16,16 +16,18 @@ window.onload=function() {
         for (var i = 0; i < 8;++i){
             var fila = document.createElement('div');
             fila.classList.add("fila");
+            fila.classList.add(i+1);
             fila.style.display = 'flex';
             fila.style.flexDirection = i % 2 === 0 ? '' : 'row-reverse'; 
             for (var j = 0 ; j < 8; ++j){
                 var casilla = document.createElement('div');
                 casilla.classList.add("casilla");
+                casilla.classList.add(letras[j] + (i+1));
                 casilla.style.width = '100' + 'px';
                 casilla.style.flexGrow = '1';
                 casilla.style.display = 'flex';
                 casilla.style.backgroundColor = j % 2 ===0 ? 'white' : 'black'; 
-                casilla.append("hola");
+                
                 fila.appendChild(casilla);
             }
            
@@ -34,48 +36,62 @@ window.onload=function() {
 
         }
     }
-
-    async function leerJSON() {
-        const respuesta = await fetch("../json/piezas.json");
-        return (await respuesta.json());
+    
+    function leerJSON() {
+        return fetch('../json/piezas.json')
+            .then(function(res) {
+                return res.json();
+            })
+            .then(function(data) {
+                console.log(data);
+                return data;
+            })
+            .catch(function(err) {
+                console.error("Error al cargar el archivo necesario: ", err);
+                return Promise.reject(err)
+            })
     }
-
-    let colocarPiezas = async() => {
-        piezas = await leerJSON();
+    function colocarPiezas() {
+        let piezas = leerJSON();
         var filas = document.querySelectorAll(".fila");
         for (var i=0;i<filas.length;++i){
-            if(filas.length-i===6){
-                
-                let imagen = document.createElement('img');
-                imagen.src=`../imagenes/${piezas[0].url}`;
-
-                var casillas = document.querySelectorAll(".fila .casilla");
-                for (var casilla of casillas ){
-                    casilla.append(imagen);
+            if(filas[i].className === "fila 7"){
+                filas[i].children;
+                for(let pieza of piezas){
+                    if(pieza.id === "1"){
+                        let imagen = document.createElement('img');
+                        imagen.src = `../imagenes/${pieza.url}`;
+                        imagen.alt = pieza.nombre;
+                    }
+                    
                 }
+                
             }
         }
-
     }
-
-    // function colocarPiezas() {
-    //     // const fs = require('fs');
-    //     // const piezas = fs.readFileSync('../../json/piezas.json');
-    //     // const piezasLeidas = JSON.parse(piezas);
+    // let colocarPiezas = async() => {
+    //     let piezas = await leerJSON();
     //     var filas = document.querySelectorAll(".fila");
     //     for (var i=0;i<filas.length;++i){
-    //         if(filas.length-i===6){
-                
-    //             let imagen = document.createElement('img');
-    //             imagen.src=piezasLeidas[0].url;
-
-    //             var casillas = document.querySelectorAll(".fila .casilla");
-    //             for (var casilla of casillas ){
-    //                 casilla.append(imagen);
+    //         if(filas[i].className === "fila 7"){
+    //             filas[i].children;
+    //             for(let pieza of piezas){
+    //                 if(pieza.id === "1"){
+    //                     let imagen = document.createElement('img');
+    //                     imagen.src = `../imagenes/${pieza.url}`;
+    //                     imagen.alt = pieza.nombre;
+    //                 }
+    //                 for (var casilla of casillas ){
+    //                     casilla.append(imagen);
+    //                 }
     //             }
+                
     //         }
     //     }
+
     // }
+
+    
     crearTablero();
     colocarPiezas();
 
